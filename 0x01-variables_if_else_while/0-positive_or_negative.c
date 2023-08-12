403510c234dcf2ed7317b3b69f062234d5172cc7
Kayode Daramola <daramola4082@gmail.com>
2023-08-12 06:47:08 -0700

    correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ if (n > 0)
 {printf("%d is positive\n", n);
 }
 else if (n == 0)
-{printf("is zero\n");
+  {printf("%d is zero\n", n);
 }
 else
-{printf(" %d is negative\n", n);
+{printf("%d is negative\n", n);
 }
 return (0);
 }
