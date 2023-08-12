f41298619249d4aaead94afe4e12510e981dc0ff
Kayode Daramola <daramola4082@gmail.com>
2023-08-11 23:56:26 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	return (puts("Programming is like building a multilingual puzzle"), 0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
