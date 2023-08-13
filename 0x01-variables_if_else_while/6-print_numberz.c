a62e9ebbdfdbe45cc1e8c9c635481d2cc38f24e3
Kayode Daramola <daramola4082@gmail.com>
2023-08-13 07:29:41 -0700

    okay

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+File Edit Options Buffers Tools C Help                                                                                                                                                                            
+#include <stdio.h>
+
+/**                                                                                                                                                                                                               
+ * main - Entry point                                                                                                                                                                                             
+ *                                                                                                                                                                                                                
+ * Description: Prints single-digit numbers in base 10                                                                                                                                                            
+ * Return: Always 0 (Success)                                                                                                                                                                                     
+ */
+int main(void)
+{
+int digit = 0;
+char digitchar;
+while (digit < 10)
+{
+digitchar = digit + '0';
+putchar(digitchar);
+digit++;
+}
+
+putchar('\n');
+
+return (0);
+}
+
