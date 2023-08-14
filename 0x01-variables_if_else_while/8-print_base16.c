53a44d266f7e4ad249e69661a49270fb8d5ac2cb
Kayode Daramola <daramola4082@gmail.com>
2023-08-14 03:41:32 -0700

    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,14 @@
  *              followed by a new line.
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 int i;
 for (i = 0; i < 10; i++) {
 putchar(i + '0');
 }
-for (i = 0; i < 6; i++) {
+for (i = 0; i < 6; i++)
+{
 putchar(i + 'a');
 }
 putchar('\n');
