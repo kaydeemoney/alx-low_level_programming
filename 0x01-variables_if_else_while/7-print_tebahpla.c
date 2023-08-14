2176d6bacd7882ae29b93df79a12ee1298d9f5ef
Kayode Daramola <daramola4082@gmail.com>
2023-08-14 02:46:48 -0700

    ok

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints the lowercase alphabet in reverse, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+char alphe;
+
+
+alphe = "abcdefghijklmnopqrstuvwxyz";
+int num_of_alphe = 24
+
+while (int num_of_alphe < 0) 
+{putchar(alphe[int num_of_alphe]);
+int num_of_alphe = int num_of_alphe - 1;
+}
+
+putchar(10);
+
+
+
+return (0);
+}
