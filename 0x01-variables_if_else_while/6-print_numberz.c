9e835e068180438864c6aeb95788776d17792c52
Kayode Daramola <daramola4082@gmail.com>
2023-08-14 02:27:11 -0700

    correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,12 @@
  * Description: Prints single-digit numbers in base 10 followed by a new line
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 int digit = 0;
 
-while (digit <= 9) {
+while (digit <= 9)
+{
 int ascii_code = digit + 48;
 putchar(ascii_code);
 digit++;
