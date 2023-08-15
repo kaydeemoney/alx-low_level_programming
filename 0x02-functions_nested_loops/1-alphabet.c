9e375921105e5259807e1b681a1ba5c45a55c5a2
Kayode Daramola <daramola4082@gmail.com>
2023-08-15 04:51:58 -0700

    again

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
-/**                                                                                                                                                                                                               
- * print_alphabet - Prints the lowercase alphabet followed by a new line                                                                                                                                          
- */
- 
+
 void print_alphabet(void);
-/**                                                                                                                                                                                                              \
-                                                                                                                                                                                                                  
- * main - Entry point of the program                                                                                                                                                                             \
-                                                                                                                                                                                                                  
- *                                                                                                                                                                                                               \
-                                                                                                                                                                                                                  
- * Return: Always 0 (Success)                                                                                                                                                                                    \
-                                                                                                                                                                                                                  
- */
 
 int main(void)
 {
-print_alphabet();
-return (0);
+    print_alphabet();
+    return 0;
 }
 
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-putchar(letter);
-letter++;
-}
-putchar('\n');
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
 }
