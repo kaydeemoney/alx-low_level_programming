a066ae6ccb5e7558de475c68380026de1beb3dd4
Kayode Daramola <daramola4082@gmail.com>
2023-08-16 04:56:13 -0700

    adding

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is below or above zero(=ve or -ve)
+* main - tests function that prints if integer is positive or negative
+* Return: 0
 */
 
 int main(void)
