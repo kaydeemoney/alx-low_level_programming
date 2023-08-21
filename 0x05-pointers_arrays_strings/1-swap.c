c409696045dfedfef8431317094c5a92f20851d8
Kayode Daramola <daramola4082@gmail.com>
2023-08-21 10:06:33 -0700

    hope betty lets this go lol

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,27 @@
+#include "main.h";
+#include <stdio.h>
+
+/**
+ * swap_int - function to swap the value of two variables
+ * @a: first pointer variable
+ * @b: second pointer variable
+ * @c: swap location of first pointer variable
+ * @d: swap location of second pointer variable
+ */
+
+
+
+
+void swap_int(int *a, int *b)
+{int *c=0;
+int *d=0;
+int *a;
+int *b;
+
+int *c = int *a;
+int *d = int *b;
+
+int *b=int *c;
+int *a =int *d;
+
+}
