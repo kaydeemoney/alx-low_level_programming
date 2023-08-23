d6ce609c346accc5fb28e6c4c34f3686501441ea
Kayode Daramola <daramola4082@gmail.com>
2023-08-22 19:19:02 -0700

    o

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - function that prints a character
+ * from every two adjacent string
+ * @str:string parameter
+ */
+
+void puts2(char *str)
+{
+	int a, p;
+	p = 0;
+	while (str[p] != 0)
+	{
+		p++;
+	}
+	for (a = 0; a < p; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * puts_half - function that would
+ * print the second half of a list
+ * as well as d last number if odd
+ * @str:string parameter
+ */
+
+void buts_half(char *str)
+{
+	int b, n, c;
+	b = 0;
+	while (str[b] != '\0')
+	{
+		b++;
+	}
+
+	if (b % 2 == 0)
+	{
+		for (c = b / 2; str[c] != '\0'; c++)
+		{
+			_butchar(str[c]);
+		}
+	}
+
+	else if (b % 2 != 0)
+	{
+		for (n = (b - 1) / 2; n < b - 1; n++)
+		{
+			_butchar(str[n + 1]);
+		}
+	}
+	_butchar('\n');
+}
