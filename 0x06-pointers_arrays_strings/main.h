a50a5e3653fd93985c6ef1f083a11b7a0cbdb8a0
Kayode Daramola <daramola4082@gmail.com>
2023-08-24 06:32:27 -0700

    o

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *_strncpy(char *dest, char *src, int n);
+char *rot13(char *);
+char *_strcat(char *dest, char *src);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+#endif
