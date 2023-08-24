2f7ba2b9d34bfea14f7097184daf1dedea1af498
Kayode Daramola <daramola4082@gmail.com>
2023-08-24 06:51:24 -0700

    o

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,22 +10,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a, j;
+	int i, j;
 
-	a = 0;
-	b = 0;
+	i = 0;
+	j = 0;
 
-	while (dest[a] != '\0')
-		a++;
+	while (dest[i] != '\0')
+		i++;
 
-	while (src[b] != '\0' && b < n)
+	while (src[j] != '\0' && j < n)
 	{
-		dest[a] = src[b];
-		a++;
-		b++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 
-	dest[a] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
