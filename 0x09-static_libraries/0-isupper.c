57e4340d4a9cfdc966be7355112117090f85050d
kaydeemoney <daramola4082@gmail.com>
2023-10-28 06:59:27 -0700

    committing before creating the library

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - check for the uppercase character
+ * @c: the character to be checked
+ * Return: 1 if c is an uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+this is my readme file on static library, coming back to update
