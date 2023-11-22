1023e387c9238fcf2e0539a37ebda0257b166891
kaydeemoney <daramola4082@gmail.com>
2023-11-21 20:11:59 -0800

    o

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,20 +3,20 @@
 /**
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
- * kd
+ * 
  * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
-	long int kddiff;
+	long int diff;
 
 	while (head)
 	{
-		kddiff = head - head->next;
+		diff = head - head->next;
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (kddiff > 0)
+		if (diff > 0)
 			head = head->next;
 		else
 		{
